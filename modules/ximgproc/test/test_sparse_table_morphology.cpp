d4276e9e039a73698b6169045ee39d0bb8b9d2d3
kyaco <kyacof@gmail.com>
2025-03-08 19:19:20 +0900

    テスト追加

--- modules/ximgproc/test/test_sparse_table_morphology.cpp
+++ modules/ximgproc/test/test_sparse_table_morphology.cpp
@@ -4,12 +4,13 @@
 
 #include "test_precomp.hpp"
 #include "opencv2/ximgproc/sparse_table_morphology.hpp"
-#include "opencv2/imgproc.hpp"
 
 namespace opencv_test {
-namespace stMorph {
+namespace {
 
-TEST(ximgproc_SparseTableMorph, compare_with_original_erode)
+// todo: ユニットテスト追加、テンプレート型引数でMatType・minmax対応、サンプル追加
+
+TEST(ximgproc_StMorph_dev, compare_with_original_erode)
 {
     // preparation
     int kRadius = 15;
@@ -41,7 +42,7 @@ TEST(ximgproc_SparseTableMorph, compare_with_original_erode)
 
     // proposal
     timer.start();
-    ximgproc::stMorph::erode(src, actual, kernel); // 217ms for Elipse, kSize = 101
+    stMorph::erode(src, actual, kernel); // 217ms for Elipse, kSize = 101
     timer.stop();
     double proposalTime = timer.getTimeMilli();
 
@@ -82,16 +83,17 @@ TEST(develop, POW2RECT_COVERING)
         1, 1, 1, 1, 1, 1, 1, 1,
     };
     Mat kernel(8, 8, CV_8UC1, ary);
-    std::vector<Rect> rects = ximgproc::stMorph::genPow2RectsToCoverKernel(kernel);
+    std::vector<Rect> rects = stMorph::genPow2RectsToCoverKernel(kernel);
 
     int rate = 20;
     resize(kernel * 255, kernel, Size(), rate, rate, InterpolationFlags::INTER_NEAREST);
     cvtColor(kernel, kernel, cv::COLOR_GRAY2BGR);
     Scalar color[20]{
-        Scalar(83, 89, 73), Scalar(49, 238, 73), Scalar(220, 192, 189), Scalar(174, 207, 34), Scalar(144, 169, 187),
-        Scalar(137, 94, 76), Scalar(42, 11, 215), Scalar(113, 11, 204), Scalar(71, 124, 8), Scalar(192, 38, 8),
-        Scalar(82, 201, 8), Scalar(70, 7, 112), Scalar(166, 219, 201), Scalar(154, 173, 0), Scalar(132, 127, 139),
-        Scalar(154, 1, 68), Scalar(231, 131, 56), Scalar(206, 238, 136), Scalar(188, 78, 173), Scalar(27, 178, 206)
+        Scalar(83, 89, 73), Scalar(49, 238, 73), Scalar(220, 192, 189), Scalar(174, 207, 34),
+        Scalar(144, 169, 187), Scalar(137, 94, 76), Scalar(42, 11, 215), Scalar(113, 11, 204),
+        Scalar(71, 124, 8), Scalar(192, 38, 8), Scalar(82, 201, 8), Scalar(70, 7, 112),
+        Scalar(166, 219, 201), Scalar(154, 173, 0), Scalar(132, 127, 139), Scalar(154, 1, 68),
+        Scalar(231, 131, 56), Scalar(206, 238, 136), Scalar(188, 78, 173), Scalar(27, 178, 206)
     };
     for (int i = 0; i < rects.size(); i++)
     {
@@ -123,7 +125,7 @@ TEST(develop, PLANNING)
         std::vector<bool>{0,1,0,0,0,0,0,0},
         std::vector<bool>{1,0,0,0,0,0,0,0},
     };
-    auto res = ximgproc::stMorph::planSparseTableConstruction(map);
+    auto res = stMorph::planSparseTableConstr(map);
 
     int g = 30;
     int r = map.size();
@@ -139,7 +141,7 @@ TEST(develop, PLANNING)
     for (int i = 0; i < res.size(); i++)
     {
         auto edge = res[i];
-        if (edge.ax == ximgproc::stMorph::Dim::Row)
+        if (edge.ax == stMorph::Dim::Row)
         {
             cv::line(m, Point(edge.dimCol * g + g / 2, edge.dimRow * g + g / 2), Point(edge.dimCol * g + g / 2, (edge.dimRow + 1) * g + g / 2), Scalar(100, 100, 100), 2);
         }
@@ -154,5 +156,126 @@ TEST(develop, PLANNING)
     destroyAllWindows();
 }
 
-} //
-} // opencv_test
+
+/*
+* Common methods during tests.
+*/
+void assertArraysIdentical(InputArray ary1, InputArray ary2)
+{
+    Mat xor = ary1.getMat() ^ ary2.getMat();
+    ASSERT_EQ(cv::countNonZero(xor.reshape(1)), 0);
+}
+Mat im(int type)
+{
+    int depth = CV_MAT_DEPTH(type);
+    int ch = CV_MAT_CN(type);
+    Mat img = imread(cvtest::TS::ptr()->get_data_path() + "cv/shared/lena.png");
+    img.convertTo(img, depth);
+
+    if (ch == 1)
+    {
+        cv::cvtColor(img, img, ColorConversionCodes::COLOR_BGR2GRAY, ch);
+    }
+    return img;
+}
+Mat kn5() { return getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, Size(5, 5)); }
+Mat kn4() { return getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, Size(4, 4)); }
+Mat kn1Zero() { return Mat::zeros(1, 1, CV_8UC1); }
+Mat kn1One() { return Mat::ones(1, 1, CV_8UC1); }
+Mat knEmpty() { return Mat(); }
+Mat knZeros() { return Mat::zeros(5, 5, CV_8UC1); }
+Mat knOnes() { return Mat::ones(5, 5, CV_8UC1); }
+Mat knBig() { return getStructuringElement(cv::MorphShapes::MORPH_RECT, Size(201, 201)); }
+
+/*
+* erode regression tests.
+*/
+void erode_rgr(InputArray src, InputArray kernel, Point anchor = Point(-1, -1),
+    int iterations = 1,
+    BorderTypes bdrType = BorderTypes::BORDER_CONSTANT, Scalar& bdrVal = Scalar::all(DBL_MAX))
+{
+    Mat expected, actual;
+    erode(src, expected, kernel, anchor, iterations, bdrType, bdrVal);
+    stMorph::erode(src, actual, kernel, anchor, iterations, bdrType, bdrVal);
+    assertArraysIdentical(expected, actual);
+}
+TEST(ximgproc_StMorph_erode, regression_8UC1) { erode_rgr(im(CV_8UC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_8UC3) { erode_rgr(im(CV_8UC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_8SC1) { erode_rgr(im(CV_8SC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_8SC3) { erode_rgr(im(CV_8SC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_16UC1) { erode_rgr(im(CV_16UC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_16UC3) { erode_rgr(im(CV_16UC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_16SC1) { erode_rgr(im(CV_16SC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_16SC3) { erode_rgr(im(CV_16SC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_32SC1) { erode_rgr(im(CV_32SC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_32SC3) { erode_rgr(im(CV_32SC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_32FC1) { erode_rgr(im(CV_32FC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_32FC3) { erode_rgr(im(CV_32FC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_64FC1) { erode_rgr(im(CV_64FC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_64FC3) { erode_rgr(im(CV_64FC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_16FC1) { erode_rgr(im(CV_16FC1), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_16FC3) { erode_rgr(im(CV_16FC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_kn5) { erode_rgr(im(CV_8UC3), kn5()); }
+TEST(ximgproc_StMorph_erode, regression_kn4) { erode_rgr(im(CV_8UC3), kn4()); }
+TEST(ximgproc_StMorph_erode, regression_kn1Zero) { erode_rgr(im(CV_8UC3), kn1Zero()); }
+TEST(ximgproc_StMorph_erode, regression_kn1One) { erode_rgr(im(CV_8UC3), kn1One()); }
+TEST(ximgproc_StMorph_erode, regression_knEmpty) { erode_rgr(im(CV_8UC3), knEmpty()); }
+TEST(ximgproc_StMorph_erode, regression_knZeros) { erode_rgr(im(CV_8UC3), knZeros()); }
+TEST(ximgproc_StMorph_erode, regression_knOnes) { erode_rgr(im(CV_8UC3), knOnes()); }
+TEST(ximgproc_StMorph_erode, regression_knBig) { erode_rgr(im(CV_8UC3), knBig()); }
+TEST(ximgproc_StMorph_erode, regression_ancMid) { erode_rgr(im(CV_8UC3), kn5(), Point(-1, -1)); }
+TEST(ximgproc_StMorph_erode, regression_ancEdge1) { erode_rgr(im(CV_8UC3), kn5(), Point(0, 0)); }
+TEST(ximgproc_StMorph_erode, regression_ancEdge2) { erode_rgr(im(CV_8UC3), kn5(), Point(4, 4)); }
+TEST(ximgproc_StMorph_erode, regression_it0) { erode_rgr(im(CV_8UC3), kn5(), Point(-1, -1), 0); }
+TEST(ximgproc_StMorph_erode, regression_it1) { erode_rgr(im(CV_8UC3), kn5(), Point(-1, -1), 1); }
+TEST(ximgproc_StMorph_erode, regression_it2) { erode_rgr(im(CV_8UC3), kn5(), Point(-1, -1), 2); }
+/*
+* erode feature tests.
+*/
+void erode_ftr(InputArray src, InputArray kernel, Point anchor = Point(-1, -1),
+    int iterations = 1,
+    BorderTypes bdrType = BorderTypes::BORDER_CONSTANT, Scalar& bdrVal = Scalar::all(DBL_MAX))
+{
+    Mat expected, actual;
+    stMorph::erode(src, actual, kernel, anchor, iterations, bdrType, bdrVal);
+    assertArraysIdentical(expected, actual);
+}
+TEST(ximgproc_StMorph_erode, feature_ancOut) { erode_ftr(im(CV_8UC3), kn5(), Point(5, 5)); }
+
+/*
+* dilate regression tests.
+*/
+void dilate_rgr(InputArray src, InputArray kernel, Point anchor = Point(-1, -1),
+    int iterations = 1,
+    BorderTypes bdrType = BorderTypes::BORDER_CONSTANT, Scalar& bdrVal = Scalar::all(DBL_MAX))
+{
+    Mat expected, actual;
+    dilate(src, expected, kernel, anchor, iterations, bdrType, bdrVal);
+    stMorph::dilate(src, actual, kernel, anchor, iterations, bdrType, bdrVal);
+    assertArraysIdentical(expected, actual);
+}
+TEST(ximgproc_StMorph_dilate, regression_8UC3) { dilate_rgr(im(CV_8UC3), kn5()); }
+TEST(ximgproc_StMorph_dilate, regression_8UC1) { dilate_rgr(im(CV_8UC1), kn5()); }
+
+/*
+* morphologyEx regression tests.
+*/
+void ex_rgr(InputArray src, MorphTypes op, InputArray kernel, Point anchor = Point(-1, -1),
+    int iterations = 1,
+    BorderTypes bdrType = BorderTypes::BORDER_CONSTANT, Scalar& bdrVal = Scalar::all(DBL_MAX))
+{
+    Mat expected, actual;
+    morphologyEx(src, expected, op, kernel, anchor, iterations, bdrType, bdrVal);
+    stMorph::morphologyEx(src, actual, op, kernel, anchor, iterations, bdrType, bdrVal);
+    assertArraysIdentical(expected, actual);
+}
+TEST(ximgproc_StMorph_ex, regression_erode) { ex_rgr(im(CV_8UC3), MORPH_ERODE, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_dilage) { ex_rgr(im(CV_8UC3), MORPH_DILATE, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_open) { ex_rgr(im(CV_8UC3), MORPH_OPEN, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_close) { ex_rgr(im(CV_8UC3), MORPH_CLOSE, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_gradient) { ex_rgr(im(CV_8UC3), MORPH_GRADIENT, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_tophat) { ex_rgr(im(CV_8UC3), MORPH_TOPHAT, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_blackhat) { ex_rgr(im(CV_8UC3), MORPH_BLACKHAT, kn5()); }
+TEST(ximgproc_StMorph_ex, regression_hitmiss) { ex_rgr(im(CV_8UC3), MORPH_HITMISS, kn5()); }
+
+}} // ::opencv_test::
